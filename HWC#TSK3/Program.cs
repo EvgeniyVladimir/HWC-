62f86e5d96b78b9bdd30feb034502b90a3969da7
Evgeniy_Vladimir <6484588@mail.ru>
2022-06-29 19:03:54 +0300

    Homework_task3

---
+++ HWC#TSK3/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        // ДЗ1 Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            
+            if (number % 2 == 0) Console.WriteLine($"Число четное - {number}");
+                else Console.WriteLine($"Число нечетное - {number}");
+        }
+    }
+}
