e7a3abe565959242e6a157aa3a64bd4a939f5672
Evgeniy_Vladimir <6484588@mail.ru>
2022-06-29 15:54:18 +0300

    Homework_C#_Task_1

---
+++ HWC#TCK1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        // ДЗ1 Написать программу, которая на вход принимает два числа и выдает какое число больше, а какое меньше.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число a:");
+            int numbera = Convert.ToInt32(Console.ReadLine( ));
+            Console.WriteLine("Введите число b:");
+            int numberb = Convert.ToInt32(Console.ReadLine( ));
+            
+            if (numbera > numberb) Console.WriteLine($"Больше число: {numbera}");
+            
+                else Console.WriteLine($"Больше число: {numberb}");
+        }
+    }
+}
