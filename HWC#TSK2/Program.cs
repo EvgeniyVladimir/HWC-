5b0fea95c0454b41e072ca632c5a53af1c669c83
Evgeniy_Vladimir <6484588@mail.ru>
2022-06-29 17:58:49 +0300

    Homework_task2

---
+++ HWC#TSK2/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        // ДЗ1 Написать программу, которая на вход принимает два числа и выдает какое число больше, а какое меньше.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число a:");
+            int numbera = Convert.ToInt32(Console.ReadLine( ));
+            Console.WriteLine("Введите число b:");
+            int numberb = Convert.ToInt32(Console.ReadLine( ));
+            Console.WriteLine("Введите число c:");
+            int numberc = Convert.ToInt32(Console.ReadLine( ));
+            int max = 0;
+
+            if (numbera > max) max = numbera;
+            if (numberb > max) max = numberb;
+            if (numberc > max) max = numberc;
+
+            Console.WriteLine($"Максимальное число: {max}");
+          }
+    }
+}
