990f2e063b4189b023eba8fe57754c27ecdc8113
Evgeniy_Vladimir <6484588@mail.ru>
2022-06-29 23:04:46 +0300

    Homework_C#_Task4

---
+++ HWC#TSK4/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        // ДЗ1 Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N. (не включая N)
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число:");
+            int N = Convert.ToInt32(Console.ReadLine());
+            int a = 1; 
+            for (a = 1; a < N; a++)
+            if (a % 2 == 0) 
+            Console.WriteLine(a);
+        }
+    }
+}
