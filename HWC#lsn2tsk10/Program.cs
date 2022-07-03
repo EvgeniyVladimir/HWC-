603af4822cdb16bd14957587449d7a6966c9565a
Evgeniy_Vladimir <6484588@mail.ru>
2022-07-03 23:37:27 +0300

    HWC#lsn2tsk10 написана

---
+++ HWC#lsn2tsk10/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        // ДЗ2_Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите трехзначное число: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            string str = number.ToString();
+            char ch = str[1];
+            Console.WriteLine(ch);
+        }
+    }
+}
