0d18892e385f9a4db79000baadcd628d2e82cdd4
Evgeniy_Vladimir <6484588@mail.ru>
2022-07-04 19:54:51 +0300

    HWC#lsn2tsk13 не выводит отсутствие третьей цифры

---
+++ HWC#lsn2tsk13/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        // ДЗ2_Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите трехзначное число: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            string str = number.ToString();
+            
+            char ch = str[2];
+            if (ch != null)
+            {
+                Console.WriteLine($"третьей цифры нет");
+            }
+            Console.WriteLine(ch);
+           
+        }
+    }
+}
