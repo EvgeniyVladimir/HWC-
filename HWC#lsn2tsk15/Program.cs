fddc755e9b5392346f0aab6bc153b4024e234276
Evgeniy_Vladimir <6484588@mail.ru>
2022-07-04 21:11:14 +0300

    HWC#lsn2tsk15 внесено предварительное решение

--- HWC#lsn2tsk15/Program.cs
+++ HWC#lsn2tsk15/Program.cs
@@ -5,16 +5,16 @@
         // ДЗ2_Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
         static void Main(string[] args)
         {
-            string 1 = "Понедельник"
-            string 2 = "Вторник"
-
-            
-            Console.WriteLine("Число дня недели: ");
+                     
+            Console.WriteLine("Введите число дня недели: ");
             int number = Convert.ToInt32(Console.ReadLine());
-            string str = number.ToString();
-            
-            
-           
+             
+            if(number < 6)
+            {
+                Console.WriteLine($"Рабочий день");
+            }
+            if (number > 5) Console.WriteLine($"Выходной день");
+                                
         }
     }
 }
