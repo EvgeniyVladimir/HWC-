0a72276cadf93ad5475eef860c2d56a24bb1d76b
Evgeniy_Vladimir <6484588@mail.ru>
2022-07-04 20:03:14 +0300

    HWC#lsn2tsk15изменение задачи

---
+++ HWC#lsn2tsk15/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        // ДЗ2_Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+        static void Main(string[] args)
+        {
+            string 1 = "Понедельник"
+            string 2 = "Вторник"
+
+            
+            Console.WriteLine("Число дня недели: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            string str = number.ToString();
+            
+            
+           
+        }
+    }
+}
