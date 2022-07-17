87de9835a4d5f58902aa94a8109310ad877dd8c5
Evgeniy_Vladimir <6484588@mail.ru>
2022-07-17 18:09:29 +0300

    HWC#lsn3tsk19 проверка палиндром

---
+++ HWC#lsn3tsk19/Program.cs
@@ -0,0 +1,34 @@
+﻿// // Задача 19
+
+// // Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// // 14212 -> нет
+// // 12821 -> да
+// // 23432 -> да
+
+namespace NomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Введите пятизначное число: ");
+            string? num = Console.ReadLine();
+            char[] a = num.ToCharArray();
+            // if(a.Length >= 4)
+            //   {
+            //     Console.WriteLine("Введите пятизначное число:");
+            //   }
+                
+            if (a[0] == a[4] && a[1] == a[3])
+              {
+                Console.WriteLine("Это палиндром");
+              }
+            else
+              {
+                Console.WriteLine("Это не палиндром");
+              }
+                
+        }
+    }
+}
