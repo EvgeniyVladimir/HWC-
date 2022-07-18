45361b0b9fe771f3355b1dbd0cfec5107dad681d
Evgeniy_Vladimir <6484588@mail.ru>
2022-07-19 01:08:31 +0300

    HWC#lsn3tsk25 возведение А в степень В

---
+++ HWC#lsn3tsk25/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+namespace NomeWorkTask25
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Введите первое число");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите второе число");
+            int b = Convert.ToInt32(Console.ReadLine());
+            // Console.WriteLine(Math.Pow(a,b));
+            int count = b;
+            int number = a;
+            for (int i = 1; i < count; i++)
+            {
+                number = number * a;
+            }
+            Console.WriteLine(number);
+        }
+    }
+}
