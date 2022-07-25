45326d071c876b103544063b06fdd24516ddbf22
Evgeniy_Vladimir <6484588@mail.ru>
2022-07-26 00:24:58 +0300

    HWC#lsn3tsk27

---
+++ HWC#lsn3tsk27/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+namespace NomeWorkTask27
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Введите число");
+            string? str = (Console.ReadLine());
+            int sum = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                sum +=
+                int.Parse((str[i]).ToString());
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ HWC#lsn3tsk29/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 29: Напишите программу, которая задаёт случайный массив случайного размера (от 5 до 10) элементов 
+// (значение элементов от 1 до 40) и выводит на экран массив квадратов этих чисел.
+// 1, 2, 5, 7, 19 -> [2, 4, 25, 49, 361]
+// 6, 1, 33 -> [36, 1, 1089]
+namespace NomeWorkTask29
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Console.Clear();
+            // Console.WriteLine("Введите число");
+            // string? str = (Console.ReadLine());
+            // int sum = 0;
+            // for (int i = 0; i < str.Length; i++)
+            // {
+            //     sum +=
+            //     int.Parse((str[i]).ToString());
+            // }
+            // Console.WriteLine(sum);
+        }
+    }
+}
